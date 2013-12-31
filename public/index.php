7160cc489d486c67a95eb2ca27bb1ba05578a7f6
vagrant <vagrant@vagrant-debian-squeeze.vagrantup.com>
2013-12-31 14:41:28 -0200

    Added the like button

--- public/index.php
+++ public/index.php
@@ -16,12 +16,22 @@
       ga('send', 'pageview');
 
     </script>
-    
+
 </head>
 <body>
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=327198550753733";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
     <div id="container">
         <div class="row center" >
-            <img src="logo.png" alt="Happy 2014"/>
+            <img src="logo.png" alt="Happy 2014"/><br/>
+            <div class="fb-like" data-href="http://happy2014.broga.com.br/" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
         </div>
 
         <div class="row center">
