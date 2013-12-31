b3a726038302740ed9dbd3667fb3047e6351af24
vagrant <vagrant@vagrant-debian-squeeze.vagrantup.com>
2013-12-31 15:14:18 -0200

    Simple mobile support

--- public/saveimage.php
+++ public/saveimage.php
@@ -1,18 +1,25 @@
 <?php
 
+$photoPath = 'pics/';
+$date = new \DateTime();
+
+$file = $photoPath . $date->format('YmdHis') . '.png';
+
 if(isset($_POST['img'])){
 
 	$img = $_POST['img'];
-
-	$photoPath = 'pics/';
-	$date = new \DateTime();
-
+	
 	$img = str_replace('data:image/png;base64,', '', $img);
 	$img = str_replace(' ', '+', $img);
 	$data = base64_decode($img);
-	$file = $photoPath . $date->format('YmdHis') . '.png';
 	$success = file_put_contents($file, $data);
 	$success ? $file : 'Unable to save the file.';
 	echo json_encode($success);
 	exit;
+	
+}else if(isset($_FILES['file'])){
+	
+	$success = file_put_contents($file, file_get_contents($_FILES['file']['tmp_name']) );
+	$success ? $file : 'Unable to save the file.';
+	header("Location: /");
 }
